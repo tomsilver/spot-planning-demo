22ec211db22c8ba54ee471fc550ebb35b31854c3
Tom Silver <tomssilver@gmail.com>
2025-08-16 12:26:32 -0400

    wip

---
+++ src/spot_planning_demo/structs.py
@@ -0,0 +1,31 @@
+"""Common data structures."""
+
+from dataclasses import dataclass
+from pybullet_helpers.geometry import Pose
+
+
+BANISH_POSE = Pose((-10000, -10000, -10000))
+
+
+class SpotAction:
+    """An executable action."""
+    
+
+@dataclass(frozen=True)
+class MoveBase(SpotAction):
+    """An action representing moving the robot base."""
+    pose: Pose  # absolute pose in world frame
+
+
+@dataclass(frozen=True)
+class Pick(SpotAction):
+    """Pick an object."""
+
+    object_name: str  # unique object name
+
+
+@dataclass(frozen=True)
+class HandOver(SpotAction):
+    """Pick an object."""
+
+    pose: Pose  # absolute pose in world frame
