d16040e91c2e4be39906170aee4cae8e1d5c927a
Tom Silver <tomssilver@gmail.com>
2025-08-25 16:00:07 -0400

    wip

--- tests/envs/test_spot_real_env.py
+++ tests/envs/test_spot_real_env.py
@@ -14,21 +14,50 @@ runreal = pytest.mark.skipif("not config.getoption('runreal')")
 
 
 @runreal
-def test_spot_real_move_base():
-    """Tests for MoveBase()."""
+def test_spot_real_custom_action_sequence():
+    """Tests a custom action sequence."""
     env = SpotRealEnv()
+    spec = env.scene_description
     env.reset(seed=123)
 
     # Create a GUI visualizer.
     env = SpotSimVizWrapper(env)
 
-    # Move to the origin.
-    move_to_origin = MoveBase(Pose.from_rpy((2.287, -0.339, 0), (0, 0, 1.421)))
+    # Pick the toy tiger. Assumes that it is in view.
+    pick_tiger = Pick(TIGER_TOY_OBJECT.name)
+    env.step(pick_tiger)
+
+    # Move back to the origin.
+    move_to_origin = MoveBase(spec.robot_base_pose)
     env.step(move_to_origin)
 
+
+@runreal
+def test_spot_real_move_base():
+    """Tests for MoveBase()."""
+    env = SpotRealEnv()
+    spec = env.scene_description
+    env.reset(seed=123)
+
+    # Create a GUI visualizer.
+    env = SpotSimVizWrapper(env)
+
     # Move backward a little bit.
-    move_backward = MoveBase(Pose.from_rpy((2.287, -0.839, 0), (0, 0, 1.421)))
+    move_backward = MoveBase(
+        Pose(
+            (
+                spec.robot_base_pose.position[0],
+                spec.robot_base_pose.position[1] - 0.5,
+                spec.robot_base_pose.position[2],
+            ),
+            spec.robot_base_pose.orientation,
+        )
+    )
     env.step(move_backward)
+
+    # Move to the origin.
+    move_to_origin = MoveBase(spec.robot_base_pose)
+    env.step(move_to_origin)
 
     # Uncomment to debug.
     # import pybullet as p
